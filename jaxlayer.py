c53c0d68dcaaa7cbb701236a01d819752e2af424
Hendrik Heine <hendrikheine@gmail.com>
2023-01-20 14:31:28 +0100

    JaxLayer working for multiple outputs

--- jaxlayer.py
+++ jaxlayer.py
@@ -7,7 +7,18 @@ def unstack_and_apply(inputs, func):
     inputs = inputs.reshape(-1,shp[-1])
     #inputs = list(inputs)
     output = [j2t(func(t2j(x))) for x in inputs]
-    return torch.stack(output).reshape(shp)
+    output = torch.stack(output)
+    return output.reshape((shp[0],shp[1],-1))
+def unstack_and_gradient(inputs, grad_func, grad):
+    shp = inputs.shape
+    inputs = inputs.reshape(-1,shp[-1])
+    grad = grad.reshape(-1,grad.shape[-1])
+    #inputs = list(inputs)
+    output = [torch.matmul(grad[i,...], j2t(grad_func(t2j(inputs[i,...])))) for i in range(inputs.shape[0])]
+    #output = [j2t(grad_func(t2j(x))) for x in inputs]
+    output = torch.stack(output)
+    #output = torch.bmm(output,grad)
+    return output.reshape((shp[0],shp[1],-1))
 def tensor_to_array(tensor):
     #temp = np.array(tensor)
     return jax.numpy.asarray(tensor)
@@ -23,29 +34,18 @@ def make_jax_function(qnode,grad):
             @staticmethod
             def backward(ctx, grad_output):
                 input, =ctx.saved_tensors
-                new_grad = unstack_and_apply(input, grad)
-                return torch.mul(grad_output,new_grad)
+                return unstack_and_gradient(input, grad, grad_output)
+                #return torch.mul(grad_output,new_grad)
     return JaxFunction
 
 class JaxLayer(torch.nn.Module):
     def __init__(self,circuit):
         super().__init__()
-        qnode = circuit.qnode
-        grad = jax.jit(jax.grad(qnode))
+        qnode = self._make_function(circuit.qnodes)
+        grad_array = [jax.jit(jax.grad(x)) for x in circuit.qnodes]
+        grad = self._make_function(grad_array)
         self.Func = make_jax_function(qnode,grad)
+    def _make_function(self,func_list):
+        return lambda x: jax.numpy.asarray([func(x) for func in func_list])
     def forward(self,inputs):
         return self.Func.apply(inputs)
-
-
-class TorchJaxLayer(torch.nn.Module):
-    def __init__(self,circuit):
-        super().__init__()
-        self.function = jax2torch(circuit.qnode)
-    def forward(self,inputs):
-        return unstack_and_apply(inputs,self.function)
-
-class Torch2JaxLayer(torch.nn.Module):
-    def __init__(self,circuit):
-        super.__init__()
-        self.function = circuit.qnode
-        self.grad = jax.jit(jax.grad(self.function))
--- quantum_circuits.py
+++ quantum_circuits.py
@@ -1,21 +1,25 @@
 import pennylane as qml
+import numpy as np
 import jax
 class SimpleQuantumCircuit:
-    def __init__(self, n_qubits, n_layers):       
+    def __init__(self, n_qubits, n_layers, n_measurements = 0):
+        if n_measurements == 0:
+            n_measurements = n_qubits
         dev = qml.device('default.qubit.jax', wires=n_qubits)
-        self.qnode = self._make_qnode(n_qubits,dev)
-        self.weight_shapes = self._make_weight_shapes(n_qubits,n_layers)
-    def _make_qnode(self, n_qubits, dev):
-        @qml.qnode(dev, interface='jax')
-        def qnode(inputs, weights):
+        weights = self._get_weights((n_layers,n_qubits))
+        self.qnodes = [self._make_qnode(n_qubits,dev,weights,i) for i in range(n_measurements)]
+        #self.qnode = self._make_qnode(n_qubits,dev, self._get_weights((n_layers,n_qubits)))
+    def _make_qnode(self, n_qubits, dev, weights, i):
+        @jax.jit
+        @qml.qnode(dev, interface='jax', shots=None, diff_method='best')
+        def qnode(inputs):
             qml.AngleEmbedding(inputs, wires=range(n_qubits))
             qml.BasicEntanglerLayers(weights, wires=range(n_qubits))
-            return [qml.expval(qml.PauliZ(wires=i)) for i in range(n_qubits)]
+            #return [qml.expval(qml.PauliZ(wires=i)) for i in range(n_qubits)]
+            return qml.expval(qml.PauliZ(wires=i))
         return qnode
-    def _make_weight_shapes(self, n_qubits, n_layers):
-        return {"weights": (n_layers, n_qubits)}
-    def get_module(self):
-        return qml.qnn.TorchLayer(self.qnode,self.weight_shapes)
+    def _get_weights(self, shape):
+        return 2*np.pi*np.random.random_sample(shape)
 class TestCircuit:
     def __init__(self):
         dev = qml.device("default.qubit.jax", wires=1, shots=None)
@@ -26,8 +30,7 @@ class TestCircuit:
             qml.Rot(inputs[0], inputs[1], inputs[2], wires=0)
             return qml.expval(qml.PauliZ(0))
         self.qnode=circuit
-    def get_module(self):
-        return qml.qnn.TorchLayer(self.qnode,{})
+        self.qnodes = [self.qnode]
 
 
 
