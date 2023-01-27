629d017409f61cd7f5b9b8b818daede59f5fa57b
Hendrik Heine <hendrikheine@gmail.com>
2023-01-27 15:52:09 +0100

    First test of reupload

--- quantum_circuits.py
+++ quantum_circuits.py
@@ -34,4 +34,28 @@ class SimpleQuantumCircuit(QuantumCircuit):
         return qnode
     def _get_weights(self, shape):
         return 2*np.pi*np.random.random_sample(shape)
-
+class ReuploadingQuantumCircuit(QuantumCircuit):
+    def __init__(self, n_qubits=4, n_uploads=3, n_layers=2, n_measurements = 0):
+        super().__init__()
+        if n_measurements == 0:
+            n_measurements = n_qubits
+        if n_measurements > n_qubits or n_measurements < 0:
+            raise ValueError("Invalid number of measurements")
+        self.n_inputs = n_qubits
+        self.n_outputs = n_measurements
+        dev = qml.device('default.qubit.jax', wires=n_qubits)
+        weights = [self._get_weights((n_layers,n_qubits)) for i in range(n_uploads)]
+        #self.qnodes = [self._make_qnode(n_qubits,dev,weights,i) for i in range(n_measurements)]
+        self.qnode = self._make_qnode(n_qubits,dev,n_uploads,weights,n_measurements)
+    def _make_qnode(self, n_qubits, dev,n_uploads, weights, n_measurements):
+        @jax.jit
+        @qml.qnode(dev, interface='jax', shots=None, diff_method='best')
+        def qnode(inputs):
+            for i in range(n_uploads):
+                qml.AngleEmbedding(inputs, wires=range(n_qubits))
+                qml.BasicEntanglerLayers(weights[i], wires=range(n_qubits))
+            return [qml.expval(qml.PauliZ(wires=i)) for i in range(n_measurements)]
+            #return qml.expval(qml.PauliZ(wires=i))
+        return qnode
+    def _get_weights(self, shape):
+        return 2*np.pi*np.random.random_sample(shape)
