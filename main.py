eef17cf63bd469e02c87237eacd111cb51de37dd
Hendrik Heine <hendrikheine@gmail.com>
2023-03-10 16:26:27 +0100

    Consistency change

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import seaborn
 from InputReader import InputReader
 if __name__ == "__main__":
     seaborn.set_style("whitegrid")
-    reader = InputReader("config5.yaml")
+    reader = InputReader("config.yaml")
     handler = reader.load_config()
     handler.full_process()
